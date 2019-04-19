c3639eda56623bff3375be5cce70e5b147fa1a34
Krystian Hebel <krystian.hebel@3mdeb.com>
2019-04-19 16:58:44 +0200

    mainboard/pcengines/apu2/state_machine.c: remove commented code
    Signed-off-by: Krystian Hebel <krystian.hebel@3mdeb.com>

--- src/mainboard/pcengines/apu2/state_machine.c
+++ src/mainboard/pcengines/apu2/state_machine.c
@@ -31,13 +31,6 @@ void platform_BeforeInitEarly(struct sysinfo *cb, AMD_EARLY_PARAMS *Early)
 
 void platform_BeforeInitPost(struct sysinfo *cb, AMD_POST_PARAMS *Post)
 {
-	//~ AGESA_STATUS status;
-
-	//~ if (CONFIG(ENABLE_MRC_CACHE)) {
-		//~ status = OemInitResume(&Post->MemConfig.MemContext);
-		//~ if (status == AGESA_SUCCESS)
-			//~ Post->MemConfig.MemRestoreCtl = 1;
-	//~ }
 }
 
 void platform_AfterInitPost(struct sysinfo *cb, AMD_POST_PARAMS *Post)
@@ -47,7 +40,6 @@ void platform_AfterInitPost(struct sysinfo *cb, AMD_POST_PARAMS *Post)
 
 void platform_BeforeInitResume(struct sysinfo *cb, AMD_RESUME_PARAMS *Resume)
 {
-	//~ OemInitResume(&Resume->S3DataBlock);
 }
 
 void platform_AfterInitResume(struct sysinfo *cb, AMD_RESUME_PARAMS *Resume)
@@ -65,7 +57,6 @@ void platform_AfterInitEnv(struct sysinfo *cb, AMD_ENV_PARAMS *Env)
 
 void platform_BeforeS3LateRestore(struct sysinfo *cb, AMD_S3LATE_PARAMS *S3Late)
 {
-	//~ OemS3LateRestore(&S3Late->S3DataBlock);
 }
 
 void platform_AfterS3LateRestore(struct sysinfo *cb, AMD_S3LATE_PARAMS *S3Late)
@@ -84,7 +75,6 @@ void platform_AfterInitLate(struct sysinfo *cb, AMD_LATE_PARAMS *Late)
 
 void platform_AfterS3Save(struct sysinfo *cb, AMD_S3SAVE_PARAMS *S3Save)
 {
-	//~ OemS3Save(&S3Save->S3DataBlock);
 }
 
 AGESA_STATUS
